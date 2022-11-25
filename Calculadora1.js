94090ca97cdc603c1d7107bdda638caa5cfb8795
carloscardenas0 <ccardenas25@ucol.mx>
2022-11-25 08:09:04 -0600

    ok

---
+++ Calculadora1.js
@@ -0,0 +1,15 @@
+class Calculadora{
+    sumar(num1, num2){
+        return num1 + num2;
+    }
+    resta(num1, num2){
+        return num1 - num2;
+    }
+    division(num1, num2){
+        return num1 / num2;
+    }
+    multiplicacion(num1, num2){
+        return num1 * num2;
+    }
+
+}
